b7ec918c25962defdcf25f75f35918dd4e6dd3f4
Jdavies91 <Jdavies86@my.bcit.ca>
2019-04-01 12:17:34 -0700

    The Sql Writer and Sql Table are paritally done. Working on finishing credentials and tables

---
+++ plugin/providers/keboola/resource_keboola_sqlserver_writer.go
@@ -0,0 +1,442 @@
+package keboola
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/url"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/plmwong/terraform-provider-keboola/plugin/providers/keboola/buffer"
+)
+
+type SQLServerWriterTableItem struct {
+	Name         string `json:"name"`
+	DatabaseName string `json:"dbName"`
+	Type         string `json:"type"`
+	Size         string `json:"size"`
+	IsNullable   bool   `json:"nullable"`
+	DefaultValue string `json:"default"`
+}
+type SQLServerWriterTable struct {
+	DatabaseName string                     `json:"dbName"`
+	Export       bool                       `json:"export"`
+	Incremental  bool                       `json:"incremental"`
+	TableID      string                     `json:"tableId"`
+	PrimaryKey   []string                   `json:"primaryKey,omitempty"`
+	Items        []SQLServerWriterTableItem `json:"items"`
+}
+type SQLServerWriter struct {
+	ID            string                       `json:"id, omitempty"`
+	Name          string                       `json:"name"`
+	Description   string                       `json:"description"`
+	Configuration SQLServerWriterConfiguration `json:"configuration"`
+}
+
+type SQLServerWriterParameters struct {
+	Database SQLServerWriterDatabaseParameters `json:"db"`
+	Tables   []SQLServerWriterTable            `json:"tables,omitempty"`
+}
+type SQLServerWriterStorageTable struct {
+	Source      string   `json:"source"`
+	Destination string   `json:"destination"`
+	Columns     []string `json:"columns"`
+}
+
+type SQLServerWriterStorage struct {
+	Input struct {
+		Tables []SQLServerWriterStorageTable `json:"tables,omitempty"`
+	} `json:"input,omitempty"`
+}
+type SQLServerWriterConfiguration struct {
+	Parameters SQLServerWriterParameters `json:"parameters"`
+	Storage    SQLServerWriterStorage    `json:"storage,omitempty"`
+}
+type SQLServerWriterDatabaseParameters struct {
+	HostName          string `json:"host"`
+	Database          string `json:"database"`
+	Password          string `json:"password,omitempty"`
+	EncryptedPassword string `json:"#password,omitempty"`
+	Username          string `json:"user"`
+	//	Version           string `json:"version"`
+	//SSH  string `json:"sshtunnel"`
+	Port string `json:"port"`
+}
+
+type ProvisionSQLServerResponse struct {
+	Status      string `json:"status"`
+	Credentials struct {
+		ID       int    `json:"id"`
+		HostName string `json:"hostname"`
+		Port     int    `json:"port"`
+		Database string `json:"db"`
+		Username string `json:"user"`
+		Password string `json:"password"`
+		//	Version     string `json:"version"`
+		WorkspaceID int `json:"workspaceId"`
+	} `json:"credentials"`
+}
+
+func resourceKeboolaSQLServerWriter() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceKeboolaSQLServerWriterCreate,
+		Read:   resourceKeboolaSQLServerWriterRead,
+		Update: resourceKeboolaSQLServerWriterUpdate,
+		Delete: resourceKeboolaSQLServerWriterDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"provision_new_instance": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+				ForceNew: true,
+			},
+			"sqlserver_db_parameters": {
+				Type:     schema.TypeMap,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"hostname": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"instancename": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"port": {
+							Type:     schema.TypeInt,
+							Optional: true,
+							Default:  1433,
+						},
+						"username": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"hashed_password": {
+							Type:         schema.TypeString,
+							Required:     true,
+							Sensitive:    true,
+							ValidateFunc: validateKBCEncryptedValue,
+						},
+						"database": {
+							Type:     schema.TypeString,
+							Required: true,
+						}, /*
+							"server_version": {
+								Type:     schema.TypeInt,
+								Required: true,
+							},*/
+						"ssh_tunnel": {
+							Type:     schema.TypeBool,
+							Optional: true,
+							Default:  false,
+						},
+					},
+				},
+			},
+			"description": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+		},
+	}
+}
+
+func resourceKeboolaSQLServerWriterCreate(d *schema.ResourceData, meta interface{}) error {
+	log.Println("[INFO] Creating SQLServer Writer in Keboola.")
+
+	client := meta.(*KBCClient)
+
+	d.Partial(true)
+	createSQLServerID, err := createSQLServerWriterConfiguration(d.Get("name").(string), d.Get("description").(string), client)
+
+	if err != nil {
+		return err
+	}
+
+	d.SetPartial("name")
+	d.SetPartial("description")
+
+	err = createSQLServerAccessToken(createSQLServerID, client)
+
+	if err != nil {
+		return err
+	}
+
+	SQLServerDatabaseCredentials := d.Get("sqlserver_db_parameters").(map[string]interface{})
+	/*
+		if d.Get("provision_new_instance").(bool) == true {
+			provisionedSQLServer, err := provisionSQLServerInstance(client)
+
+			if err != nil {
+				return err
+			}
+
+			SQLServerDatabaseCredentials = map[string]interface{}{
+				"hostname":        provisionedSQLServer.Credentials.HostName,
+				"port":            strconv.Itoa(provisionedSQLServer.Credentials.Port),
+				"database":        provisionedSQLServer.Credentials.Database,
+				"version":         provisionedSQLServer.Credentials.Version,
+				"username":        provisionedSQLServer.Credentials.Username,
+				"hashed_password": provisionedSQLServer.Credentials.Password,
+			}
+		}
+	*/
+	//Need to configure configuration
+	err = createSQLServerCredentialsConfiguration(SQLServerDatabaseCredentials, createSQLServerID, client)
+
+	if err != nil {
+		return err
+	}
+
+	d.SetPartial("sqlserver_db_parameters")
+
+	d.SetId(createSQLServerID)
+
+	d.Partial(false)
+
+	return resourceKeboolaSQLServerWriterRead(d, meta)
+}
+
+func createSQLServerAccessToken(SQLServerID string, client *KBCClient) error {
+	createAccessTokenForm := url.Values{}
+	createAccessTokenForm.Add("description", fmt.Sprintf("wrdbSqlServer_%s", SQLServerID))
+	createAccessTokenForm.Add("canManageBuckets", "1")
+
+	createAccessTokenBuffer := buffer.FromForm(createAccessTokenForm)
+
+	createAccessTokenResponse, err := client.PostToStorage("storage/tokens", createAccessTokenBuffer)
+
+	if hasErrors(err, createAccessTokenResponse) {
+		return extractError(err, createAccessTokenResponse)
+	}
+
+	return nil
+}
+func createSQLServerWriterConfiguration(name string, description string, client *KBCClient) (createdSQLServerID string, err error) {
+	createWriterForm := url.Values{}
+	createWriterForm.Add("name", name)
+	createWriterForm.Add("description", description)
+
+	createWriterBuffer := buffer.FromForm(createWriterForm)
+
+	createResponse, err := client.PostToStorage("storage/components/keboola.wr-db-mssql-v2/configs", createWriterBuffer)
+
+	if hasErrors(err, createResponse) {
+		return "", extractError(err, createResponse)
+	}
+
+	var createWriterResult CreateResourceResult
+
+	decoder := json.NewDecoder(createResponse.Body)
+	err = decoder.Decode(&createWriterResult)
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(createWriterResult.ID), nil
+}
+
+func provisionSQLServerInstance(client *KBCClient) (provisionedSQLServerResponse *ProvisionSQLServerResponse, err error) {
+	provisionSQLServerBuffer := bytes.NewBufferString("{ \"type\": \"writer\" }")
+	provisionSQLServerResponse, err := client.PostToSyrup("provisioning/docker", provisionSQLServerBuffer)
+
+	if hasErrors(err, provisionSQLServerResponse) {
+		return nil, extractError(err, provisionSQLServerResponse)
+	}
+
+	var provisionedSQLServer ProvisionSQLServerResponse
+
+	provisionedSQLServerDecoder := json.NewDecoder(provisionSQLServerResponse.Body)
+	err = provisionedSQLServerDecoder.Decode(&provisionedSQLServer)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if provisionSQLServerResponse.StatusCode < 200 || provisionSQLServerResponse.StatusCode > 299 {
+		return nil, fmt.Errorf("Unable to provision Sql Server instance (status code: %v)", provisionSQLServerResponse.StatusCode)
+	}
+
+	return &provisionedSQLServer, nil
+}
+func mapSQLServerCredentialsToConfiguration(source map[string]interface{}) SQLServerWriterDatabaseParameters {
+	databaseParameters := SQLServerWriterDatabaseParameters{}
+
+	if val, ok := source["hostname"]; ok {
+		databaseParameters.HostName = val.(string)
+	}
+	if val, ok := source["port"]; ok {
+		databaseParameters.Port = val.(string)
+	}
+	if val, ok := source["database"]; ok {
+		databaseParameters.Database = val.(string)
+	}
+	/*
+		if val, ok := source["server_version"]; ok {
+			databaseParameters.Version = val.(string)
+		}
+	*/
+	if val, ok := source["username"]; ok {
+		databaseParameters.Username = val.(string)
+	}
+	if val, ok := source["hashed_password"]; ok {
+		databaseParameters.EncryptedPassword = val.(string)
+	}
+	/*
+		if val, ok := source["ssh_tunnel"]; ok {
+			databaseParameters.EncryptedPassword = val.(string)
+		}
+	*/
+	return databaseParameters
+}
+func createSQLServerCredentialsConfiguration(sqlserverCredentials map[string]interface{}, createdSQLServerID string, client *KBCClient) error {
+
+	sqlserverWriterConfiguration := SQLServerWriterConfiguration{}
+
+	sqlserverWriterConfiguration.Parameters.Database = mapSQLServerCredentialsToConfiguration(sqlserverCredentials)
+
+	sqlserverWriterConfigurationJSON, err := json.Marshal(sqlserverWriterConfiguration)
+
+	if err != nil {
+		return err
+	}
+
+	updateConfigurationRequestForm := url.Values{}
+	updateConfigurationRequestForm.Add("configuration", string(sqlserverWriterConfigurationJSON))
+	updateConfigurationRequestForm.Add("changeDescription", "Created database credentials")
+
+	updateConfigurationRequestBuffer := buffer.FromForm(updateConfigurationRequestForm)
+
+	updateConfigurationResponse, err := client.PutToStorage(fmt.Sprintf("storage/components/keboola.wr-db-mssql-v2/configs/%s", createdSQLServerID), updateConfigurationRequestBuffer)
+
+	if hasErrors(err, updateConfigurationResponse) {
+		return extractError(err, updateConfigurationResponse)
+	}
+
+	return nil
+}
+func resourceKeboolaSQLServerWriterRead(d *schema.ResourceData, meta interface{}) error {
+	log.Println("[INFO] Reading SQLServer Writers from Keboola.")
+
+	client := meta.(*KBCClient)
+	getSQLServerWriterResponse, err := client.GetFromStorage(fmt.Sprintf("storage/components/keboola.wr-db-mssql-v2/configs/%s", d.Id()))
+
+	if d.Id() == "" {
+		return nil
+	}
+
+	if hasErrors(err, getSQLServerWriterResponse) {
+		if getSQLServerWriterResponse.StatusCode == 404 {
+			d.SetId("")
+			return nil
+		}
+
+		return extractError(err, getSQLServerWriterResponse)
+	}
+
+	var sqlserverwriter SQLServerWriter
+	decoder := json.NewDecoder(getSQLServerWriterResponse.Body)
+	err = decoder.Decode(&sqlserverwriter)
+
+	if err != nil {
+		return err
+	}
+
+	d.Set("id", sqlserverwriter.ID)
+	d.Set("name", sqlserverwriter.Name)
+	d.Set("description", sqlserverwriter.Description)
+
+	if d.Get("provision_new_database") == false {
+		dbParameters := make(map[string]interface{})
+
+		databaseCredentials := sqlserverwriter.Configuration.Parameters.Database
+
+		dbParameters["hostname"] = databaseCredentials.HostName
+		dbParameters["port"] = databaseCredentials.Port
+		dbParameters["database"] = databaseCredentials.Database
+		//	dbParameters["server_version"] = databaseCredentials.Version
+		dbParameters["username"] = databaseCredentials.Username
+		dbParameters["hashed_password"] = databaseCredentials.EncryptedPassword
+		//dbParameters["ssh_tunnel"] = databaseCredentials.EncryptedPassword
+		d.Set("sqlserver_db_parameters", dbParameters)
+	}
+
+	return nil
+}
+func resourceKeboolaSQLServerWriterUpdate(d *schema.ResourceData, meta interface{}) error {
+
+	log.Println("[INFO] Updating SQLServer Writer in Keboola.")
+
+	client := meta.(*KBCClient)
+
+	getWriterResponse, err := client.GetFromStorage(fmt.Sprintf(fmt.Sprintf("storage/components/keboola.wr-db-mssql-v2/configs/%s", d.Id())))
+
+	if hasErrors(err, getWriterResponse) {
+		return extractError(err, getWriterResponse)
+	}
+
+	var sqlserverwriter SQLServerWriter
+
+	decoder := json.NewDecoder(getWriterResponse.Body)
+	err = decoder.Decode(&sqlserverwriter)
+
+	if err != nil {
+		return err
+	}
+
+	sqlserverCredentials := d.Get("sqlserver_db_parameters").(map[string]interface{})
+
+	if d.Get("provision_new_instance").(bool) == false {
+		sqlserverwriter.Configuration.Parameters.Database = mapSQLServerCredentialsToConfiguration(sqlserverCredentials)
+	}
+
+	sqlserverConfigJSON, err := json.Marshal(sqlserverwriter.Configuration)
+
+	if err != nil {
+		return err
+	}
+
+	updateCredentialsForm := url.Values{}
+	updateCredentialsForm.Add("name", d.Get("name").(string))
+	updateCredentialsForm.Add("description", d.Get("description").(string))
+	updateCredentialsForm.Add("configuration", string(sqlserverConfigJSON))
+	updateCredentialsForm.Add("changeDescription", "Updated SQL Server Writer configuration via Terraform")
+
+	updateCredentialsBuffer := buffer.FromForm(updateCredentialsForm)
+
+	updateCredentialsResponse, err := client.PutToStorage(fmt.Sprintf("storage/components/keboola.wr-db-mssql-v2/configs/%s", d.Id()), updateCredentialsBuffer)
+
+	if hasErrors(err, updateCredentialsResponse) {
+		return extractError(err, updateCredentialsResponse)
+	}
+
+	return resourceKeboolaSQLServerWriterRead(d, meta)
+}
+
+func resourceKeboolaSQLServerWriterDelete(d *schema.ResourceData, meta interface{}) error {
+
+	log.Printf("[INFO] Deleting SQL Server Writer in Keboola: %s", d.Id())
+
+	client := meta.(*KBCClient)
+	destroyResponse, err := client.DeleteFromStorage(fmt.Sprintf("storage/components/keboola.wr-db-mssql-v2/configs/%s", d.Id()))
+
+	if hasErrors(err, destroyResponse) {
+		return extractError(err, destroyResponse)
+	}
+
+	d.SetId("")
+
+	return nil
+
+}
---
+++ plugin/providers/keboola/resource_keboola_sqlserver_writer_tables.go
@@ -0,0 +1,252 @@
+package keboola
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/url"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/plmwong/terraform-provider-keboola/plugin/providers/keboola/buffer"
+)
+
+func resourceKeboolaSQLServerWriterTables() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceKeboolaSQLServerWriterTablesCreate,
+		Read:   resourceKeboolaSQLServerTablesRead,
+		Update: resourceKeboolaSQLServerWriterTablesUpdate,
+		Delete: resourceKeboolaSQLServerWriterTablesDelete,
+
+		Schema: map[string]*schema.Schema{
+			"writer_id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"table": {
+				Type:     schema.TypeSet,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"db_name": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"export": {
+							Type:     schema.TypeBool,
+							Optional: true,
+							Default:  false,
+						},
+						"table_id": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"incremental": {
+							Type:     schema.TypeBool,
+							Optional: true,
+							Default:  false,
+						},
+						"primary_key": {
+							Type:     schema.TypeList,
+							Optional: true,
+							Elem: &schema.Schema{
+								Type: schema.TypeString,
+							},
+						},
+						"column": {
+							Type:     schema.TypeList,
+							Optional: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"name": {
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+									"db_name": {
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+									"type": {
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+									"size": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"nullable": {
+										Type:     schema.TypeBool,
+										Optional: true,
+										Default:  false,
+									},
+									"default": {
+										Type:     schema.TypeString,
+										Optional: true,
+										Default:  "",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func resourceKeboolaSQLServerWriterTablesCreate(d *schema.ResourceData, meta interface{}) error {
+	log.Println("[INFO] Creating SQL Server Writer Tables in Keboola")
+	writerID := d.Get("writer_id").(string)
+	tables := d.Get("table").(*schema.Set).List()
+
+	mappedTables := make([]SQLServerWriterTable, 0, len(tables))
+	storageTables := make([]SQLServerWriterStorageTable, 0, len(tables))
+
+	for _, table := range tables {
+		config := table.(map[string]interface{})
+		mappedTable := SQLServerWriterTable{
+			DatabaseName: config["db_name"].(string),
+			Export:       config["export"].(bool),
+			TableID:      config["table_id"].(string),
+			Incremental:  config["incremental"].(bool),
+		}
+
+		if q := config["primary_key"]; q != nil {
+			mappedTable.PrimaryKey = AsStringArray(q.([]interface{}))
+		}
+
+		storageTable := SQLServerWriterStorageTable{
+			Source:      mappedTable.TableID,
+			Destination: fmt.Sprintf("%s.csv", mappedTable.TableID),
+		}
+		columnConfigs := config["column"].([]interface{})
+		mappedColumns := make([]SQLServerWriterTableItem, 0, len(columnConfigs))
+		columnNames := make([]string, 0, len(columnConfigs))
+
+		for _, column := range columnConfigs {
+			columnConfig := column.(map[string]interface{})
+
+			mappedColumn := SQLServerWriterTableItem{
+				Name:         columnConfig["name"].(string),
+				DatabaseName: columnConfig["db_name"].(string),
+				Type:         columnConfig["type"].(string),
+				Size:         columnConfig["size"].(string),
+				IsNullable:   columnConfig["nullable"].(bool),
+				DefaultValue: columnConfig["default"].(string),
+			}
+			mappedColumns = append(mappedColumns, mappedColumn)
+			columnNames = append(columnNames, mappedColumn.Name)
+		}
+		mappedTable.Items = mappedColumns
+		storageTable.Columns = columnNames
+
+		mappedTables = append(mappedTables, mappedTable)
+		storageTables = append(storageTables, storageTable)
+	}
+	client := meta.(*KBCClient)
+
+	getWriterResponse, err := client.GetFromStorage(fmt.Sprintf("storage/components/keboola.wr-db-mssql-v2/configs/%s", writerID))
+
+	if hasErrors(err, getWriterResponse) {
+		return extractError(err, getWriterResponse)
+	}
+
+	var sqlserverwriter SQLServerWriter
+
+	decoder := json.NewDecoder(getWriterResponse.Body)
+	err = decoder.Decode(&sqlserverwriter)
+
+	if err != nil {
+		return err
+	}
+	sqlserverwriter.Configuration.Parameters.Tables = mappedTables
+	sqlserverwriter.Configuration.Storage.Input.Tables = storageTables
+
+	sqlserverConfigJSON, err := json.Marshal(sqlserverwriter.Configuration)
+
+	if err != nil {
+		return err
+	}
+	updateSQLServerForm := url.Values{}
+	updateSQLServerForm.Add("configuration", string(sqlserverConfigJSON))
+	updateSQLServerForm.Add("changeDescription", "Update SQL Server tables")
+
+	updateSQLServerBuffer := buffer.FromForm(updateSQLServerForm)
+
+	updateResponse, err := client.PutToStorage(fmt.Sprintf("storage/components/keboola.wr-db-mssql-v2/configs/%s", writerID), updateSQLServerBuffer)
+
+	if hasErrors(err, updateResponse) {
+		return extractError(err, updateResponse)
+	}
+
+	d.SetId(writerID)
+
+	return resourceKeboolaSQLServerTablesRead(d, meta)
+}
+func resourceKeboolaSQLServerTablesRead(d *schema.ResourceData, meta interface{}) error {
+	log.Println("[INFO] Reading SQL Serverr Writer Tables from Keboola.")
+	if d.Id() == "" {
+		return nil
+	}
+	client := meta.(*KBCClient)
+	getSQLServerWriterResponse, err := client.GetFromStorage(fmt.Sprintf("storage/components/keboola.wr-db-mssql-v2/configs/%s", d.Id()))
+
+	if hasErrors(err, getSQLServerWriterResponse) {
+		if getSQLServerWriterResponse.StatusCode == 404 {
+			d.SetId("")
+			return nil
+		}
+
+		return extractError(err, getSQLServerWriterResponse)
+	}
+	var sqlserverWriter SQLServerWriter
+
+	decoder := json.NewDecoder(getSQLServerWriterResponse.Body)
+	err = decoder.Decode(&sqlserverWriter)
+
+	if err != nil {
+		return err
+	}
+
+	var tables []map[string]interface{}
+
+	for _, tableConfig := range sqlserverWriter.Configuration.Parameters.Tables {
+		tableDetails := map[string]interface{}{
+			"db_name":     tableConfig.DatabaseName,
+			"export":      tableConfig.Export,
+			"table_id":    tableConfig.TableID,
+			"incremental": tableConfig.Incremental,
+			"primary_key": tableConfig.PrimaryKey,
+		}
+		var columns []map[string]interface{}
+		for _, item := range tableConfig.Items {
+			columnDetails := map[string]interface{}{
+				"name":     item.Name,
+				"db_name":  item.DatabaseName,
+				"type":     item.Type,
+				"size":     item.Size,
+				"nullable": item.IsNullable,
+				"default":  item.DefaultValue,
+			}
+
+			columns = append(columns, columnDetails)
+		}
+		tableDetails["column"] = columns
+
+		tables = append(tables, tableDetails)
+	}
+
+	d.Set("table", tables)
+
+	return nil
+}
+
+func resourceKeboolaSQLServerWriterTablesUpdate(d *schema.ResourceData, meta interface{}) error {
+
+	return nil
+	//return resourceKeboolaSnowflakeWriterTablesRead(d, meta)
+}
+
+func resourceKeboolaSQLServerWriterTablesDelete(d *schema.ResourceData, meta interface{}) error {
+
+	return nil
+}
