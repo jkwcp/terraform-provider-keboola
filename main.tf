379e9e33b04fdc773b36d762d2f6e80435b8b9fe
jkwcp <jk.cpwang@gmail.com>
2019-04-07 22:55:23 -0700

    Added demo terraform, added aws s3bucket to provider.go

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 #must include api_key, look like this:
 
 #provider "keboola" {
-#  api_key = "6017-153348-I39OH0o6ZC6Gcg3hdPqIJbojCWRgb4XLgoXRS1mz"
+#  api_key = "5959-152644-8hUPlB5CRGTOLmuYWKToMQ4CWrHUcfRoogM1gkJe"
 #}
 
 #Configurations will be like:
@@ -11,6 +11,80 @@
 #resource "keboola_component_writer" "Component Name" {
 #     Configurations....  
 #}
+
 provider "keboola" {
-  api_key = "6017-153348-I39OH0o6ZC6Gcg3hdPqIJbojCWRgb4XLgoXRS1mz"
+  api_key = "5959-152644-jtfLgkQuUNaeYp1alpMt9GfDsjxb59FsUkQp6Dw4"
 }
+
+
+resource "keboola_snowflake_extractor" "DemoSnowflakeExtractor" {
+  name = "Snowflake Extractor"
+  description = "Snowflake Extractor"
+  snowflake_db_parameters{
+    hostname="kebooladev.snowflakecomputing.com"
+    port="443"
+    database="HELP_TUTORIAL"
+    schema="HELP_TUTORIAL"
+    warehouse="DEV"
+    user="HELP_TUTORIAL"
+    hashed_password="HELP_TUTORIAL"
+  }
+}
+
+resource "keboola_googledrive_extractor" "DemoGoogleDriveExtractor" {
+  name  = "Google Drive Extractor"
+  description = "Google Drive Extractor"
+}
+
+resource "keboola_transformation_bucket" "DemoTransformationBucket" {
+  name = "Transformation Bucket"
+  description = "Transformation Bucket"
+
+}
+
+resource "keboola_transformation" "DemoTransformation" {
+  bucket_id = "${keboola_transformation_bucket.DemoTransformationBucket.id}"
+  name = "Transformation"
+  description = "Transformation"
+  backend = "snowflake"
+  type = "simple"
+
+}
+
+resource "keboola_awsredshift_writer" "DemoAWSRedshiftWriter" {
+  name  = "Redshift"
+  description = "This is an example of aws Redshift"
+  redshift_wr_parameters{
+    hostname = "jondw.cs96vivrwyuv.us-west-2.redshift.amazonaws.com"
+    port = "5439"
+    username = "jon"
+    hashed_password = "Computer#18"
+    database = "jontestdb"
+    schema  = "information_schema"
+  }
+}
+
+resource "keboola_aws_s3bucket_writer" "DemoS3Bucket" {
+  name  = "S3 Bucket"
+  description = "This is an example of the S3 Bucket"
+  s3_wr_parameters {
+    accessKeyId = "arn:aws:s3:::a01011881-lab-3"
+    bucket = "a01011881-lab-3"
+    secretaccesskey = "=+hY8qbR78iXlxBKA6FzBx0rWrdfeMPbdtqHdTsJR"
+  }
+}
+
+resource "keboola_dropbox_writer" "DemoDropBoxWriter" {
+  name  = "Drop Box"
+  description = "AndyTest_Dropbox"
+}
+
+resource "keboola_tableau_writer" "DemoTableauWriter" {
+  name  = "Tableau"
+  description = "AndyTest_Tableau"
+}
+
+
+
+
+
