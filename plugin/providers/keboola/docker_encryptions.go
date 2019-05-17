4e707da097a4460f53d12978fa7c3c502aac8a4b
Jdavies91 <Jdavies86@my.bcit.ca>
2019-05-16 19:11:13 -0700

    Commented out the SSH still trying to figure out auto generation

--- plugin/providers/keboola/docker_encryptions.go
+++ plugin/providers/keboola/docker_encryptions.go
@@ -21,15 +21,20 @@ func (c *KBCClient) PostToDockerEncrypt(componentID string, jsonpayload []byte, 
 	req.Header.Add("Content-Type", "text/plain")
 	return client.Do(req)
 }
+
+/*
 func (c *KBCClient) PostToDockerCreateSSH(componentID string, jsonpayload []byte, projectID string) (*http.Response, error) {
 	client := &http.Client{}
 
-	req, err := http.NewRequest("POST", fmt.Sprintf("https://docker-runner.keboola.com/docker/encrypt?componentId=%s&projectId=%s", componentID, projectID), bytes.NewBuffer(jsonpayload))
+	body := bytes.NewReader(payloadBytes)
 
+	req, err := http.NewRequest("POST", "https://docker-runner.keboola.com/docker/keboola.ssh-keygen-v2/action/generate", body)
 	if err != nil {
-		return nil, err
+		// handle err
 	}
-	req.Header.Set("X-StorageApi-Token", c.APIKey)
-	req.Header.Add("Content-Type", "text/plain")
+	req.Header.Set("X-Storageapi-Token", "{STORAGE_API_TOKEN}")
+	req.Header.Set("Content-Type", "application/json")
+
 	return client.Do(req)
 }
+*/
